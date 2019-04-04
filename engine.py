1df49b07639f2c174340060efeecfce27c3a7d60
Ran Zelinger <Ran.zelinger@gmail.com>
2019-04-04 23:14:23 +0300

    Part 4
    Field of Vision and Fog of War for the dungeon.

--- engine.py
+++ engine.py
@@ -1,10 +1,12 @@
 import tcod as libtcod
 from input_handlers import handle_keys # Importing a function
 from entity import Entity # Importing a class
+from fov_functions import initialize_fov , recompute_fov # FoV function
 from render_functions import clear_all, render_all # Importing some functions
 from map_objects.game_map import GameMap
 
 def main():
+    # Variables for screen and map size.
     screen_width = 80
     screen_height = 50
     map_width = 80
@@ -15,10 +17,17 @@ def main():
     room_min_size = 6
     max_rooms = 30
 
+    # Variables for Field of Vision for the adventurer.
+    fov_algorithm = 0 # '0' is just the default algorithm that libtcod uses; it has more
+    fov_light_walls = True # tells us whether or not to 'light up' the walls we see
+    fov_radius = 10
+
     # Making a dictionary that holds game colors.
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
-        'dark_ground': libtcod.Color(50, 50, 150)
+        'dark_ground': libtcod.Color(50, 50, 150),
+        'light_wall': libtcod.Color(130, 110, 50),
+        'light_ground': libtcod.Color(200, 180, 50)
     }
     # We use 'int' casting since the libtcod functions require integers, not floats.
     # Creating the entities for the game..
@@ -33,13 +42,21 @@ def main():
     con = libtcod.console_new(screen_width, screen_height) # Setting up a default console..
     game_map = GameMap(map_width, map_height)
     game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+    # Flag for deciding on whether to recompute FoV.
+    fov_recompute = True
+    fov_map = initialize_fov(game_map)
+
     # Setting up user inputs. Keyboard and mouse.
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse) # This will record user inputs in the 'key' and 'mouse' variables.
-        render_all(con, entities, game_map, screen_width, screen_height, colors)
+        if fov_recompute: # Check if to recompute FoV.
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+
+        fov_recompute = False
         libtcod.console_flush() # Print all the last 'put chars' onto the console.
 
         clear_all(con, entities) # Drawing a blank on the last visited cell.. So there'll be no trail.
@@ -54,6 +71,7 @@ def main():
             dx, dy = move
             if not game_map.is_blocked(player.x + dx, player.y + dy): # Move player only if he's not blocked.
                 player.move(dx, dy)
+                fov_recompute = True
         if exit:
             return True
         if fullscreen:
--- render_functions.py
+++ render_functions.py
@@ -2,20 +2,29 @@
 import libtcodpy as libtcod
 
 
-def render_all(con, entities, game_map, screen_width, screen_height, colors):
-    # Draw all the tiles in the game map
-    for y in range(game_map.height):
-        for x in range(game_map.width):
-            wall = game_map.tiles[x][y].block_sight
+def render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors):
+    # Draw all the tiles in the game map, while considering player FoV.
+    if fov_recompute:
+        for y in range(game_map.height):
+            for x in range(game_map.width):
+                visible = libtcod.map_is_in_fov(fov_map, x, y)
+                wall = game_map.tiles[x][y].block_sight
 
-            if wall:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
-            else:
-                libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
+                if visible:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('light_ground'), libtcod.BKGND_SET)
+                    game_map.tiles[x][y].explored = True # Set tile as explored. Only explored tiles will be drawn.
+                elif game_map.tiles[x][y].explored:
+                    if wall:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_wall'), libtcod.BKGND_SET)
+                    else:
+                        libtcod.console_set_char_background(con, x, y, colors.get('dark_ground'), libtcod.BKGND_SET)
 
     # Draw all entities in the list, using the 'draw_entity' function.
     for entity in entities:
-        draw_entity(con, entity)
+        draw_entity(con, entity, fov_map)
 
     # I guess console-blit copies stuff from the first parameter ccnsole into the root console.
     libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
@@ -27,10 +36,11 @@ def clear_all(con, entities):
         clear_entity(con, entity)
 
 
-def draw_entity(con, entity):
-    # Draw things, using the entity's values.
-    libtcod.console_set_default_foreground(con, entity.color)
-    libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
+def draw_entity(con, entity, fov_map):
+    if libtcod.map_is_in_fov(fov_map, entity.x, entity.y):
+        # Draw things, using the entity's values.
+        libtcod.console_set_default_foreground(con, entity.color)
+        libtcod.console_put_char(con, entity.x, entity.y, entity.char, libtcod.BKGND_NONE)
 
 
 def clear_entity(con, entity):
